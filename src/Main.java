542932265e6d7b060d05e9fa517da33864845b41
Arthur_Starovoyt <89992433888@mail.ru>
2024-10-01 19:01:23 +0300

    Проверьте, пожалуйста, дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println(totalWeight);
+        var differenceWeight =  firstBoxerWeight - secondBoxerWeight;
+        System.out.println(differenceWeight);
+
+        var remainderWeight = secondBoxerWeight % firstBoxerWeight;
+        System.out.println(remainderWeight);
+
+
+        var openingHours = 640;
+        var employeeWorkingTime = 8;
+        var employeesCompany = openingHours / employeeWorkingTime;
+        System.out.println("Всего работников в компании — " + employeesCompany + " человек");
+
+        employeesCompany = employeesCompany + 94;
+        openingHours = openingHours + employeesCompany * employeeWorkingTime;
+
+        System.out.println("Если в компании работает " + employeesCompany + " человек(а), то всего " + openingHours +
+                " часов работы может быть поделено между сотрудниками");
+
     }
 }
+
+
